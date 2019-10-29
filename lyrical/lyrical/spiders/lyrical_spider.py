359df54905f10da52ccbba3fa75a04f99a25dbd3
bersena911 <berse911@gmail.com>
2019-10-30 00:59:34 +0400

    remove custom headers

--- lyrical/lyrical/spiders/lyrical_spider.py
+++ lyrical/lyrical/spiders/lyrical_spider.py
@@ -12,21 +12,12 @@ class LyricalSpiderSpider(scrapy.Spider):
     allowed_domains = ['azlyrics.com']
     proxy = 'http://127.0.0.1:24000'
     start_urls = []
-    headers = {
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.9,ka;q=0.8',
-    }
 
     def start_requests(self):
         self.ua = UserAgent()
-        self.headers['User-Agent'] = self.ua.random
         yield scrapy.Request(
             url='https://www.azlyrics.com',
             callback=self.parse,
-            headers=self.headers,
             meta={
                 'proxy': self.proxy
             }
@@ -35,11 +26,9 @@ class LyricalSpiderSpider(scrapy.Spider):
     def parse(self, response):
         alphabet_urls = response.xpath('//a[@class="btn btn-menu"]/@href').extract()
         for url in alphabet_urls:
-            self.headers['User-Agent'] = self.ua.random
             yield scrapy.Request(
                 url=response.urljoin(url),
                 callback=self.parse_artists,
-                headers=self.headers,
                 meta={
                     'proxy': self.proxy
                 }
@@ -50,11 +39,9 @@ class LyricalSpiderSpider(scrapy.Spider):
         for artist in artists:
             name = artist.xpath('./text()').extract_first()
             url = artist.xpath('./@href').extract_first()
-            self.headers['User-Agent'] = self.ua.random
             yield scrapy.Request(
                 url=response.urljoin(url),
                 callback=self.parse_albums,
-                headers=self.headers,
                 meta={
                     'proxy': self.proxy,
                     'artist': name
@@ -84,11 +71,9 @@ class LyricalSpiderSpider(scrapy.Spider):
                 lyrical_item['artist'] = artist
                 lyrical_item['song'] = song_name
                 lyrical_item['year'] = year
-                self.headers['User-Agent'] = self.ua.random
                 yield scrapy.Request(
                     url=response.urljoin(song_url),
                     callback=self.parse_lyrics,
-                    headers=self.headers,
                     meta={
                         'lyrical_item': lyrical_item,
                         'proxy': self.proxy
